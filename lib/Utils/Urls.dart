c7eb57751f03cdec8a0348bafa8f5a1a08848ffb
abhijith <abhijithbeegains@gmail.com>
2022-04-02 14:54:28 +0530

    removed unwanted urls

--- lib/Utils/Urls.dart
+++ lib/Utils/Urls.dart
@@ -1,42 +1,6 @@
 
 class Urls {
-  //static const BASE_URL="https://www.potafo.in/testing_new/api/";
-  //static const ImageUrl="https://www.potafo.in/testing_new/";
-  //static const BASE_URL="http://potafo.herokuapp.com/api/v1/";
-  //static const ImageUrl="http://potafo.herokuapp.com";
-  static const BASE_URL="http://13.59.135.131:8000/api/v1/";
-  static const ImageUrl="http://13.59.135.131:8000";
-  // static const BASE_URL="http://potafo.herokuapp.com/api/v1/";
-  // static const ImageUrl="http://potafo.herokuapp.com";
-  //static const BASE_URL="http://13.59.135.131:8000/api/v1/";
-  //static const ImageUrl="http://13.59.135.131:8000/";
-  static const LOGIN=BASE_URL+"auth/";
-  static const REGISTER_NEW=BASE_URL+"register/";
-  static const UPDATE_FIREBASE_TOKEN=BASE_URL+"update-token/";
-  static const NUMBER_VERIFICATION = BASE_URL+"check-phone/";
-  static const VERIFY_OTP_NEW=BASE_URL+"verify-otp/";
-  static const FORGOT_PASSWORD_NEW = BASE_URL+"forgot_password_new";
-  static const GET_RESTUARANT_DETAILS = BASE_URL+"restaurant/web/";
-  static const GET_RESTUARANT_CATEGORY = BASE_URL+"restaurant/category/";
-  static const GET_RESTUARANT_ITEMLISTS = BASE_URL+"item_lists";
-  static const ADD_RESTUARANT_CART = BASE_URL+"cart/";
-  static const GET_CART_LIST = BASE_URL+"cart/";
-  static const CLEAR_ALL_CART = BASE_URL+"cart_clear/";
-  static const HOME_BANNER = BASE_URL+"banner/";
-  static const HOME = BASE_URL+"home/";
-  static const MART_HOME = HOME+"mart/";
-  static const HOME_RESTAURANTS = BASE_URL+"restaurants_new";
-  static const RESTUARANT_ITEM_LIST = BASE_URL+"vendor-detail/";
-  static const RESTUARNT_ITEM_CAT_LIST = BASE_URL+"vendor-filter/";
-  static const OFFER_LIST = BASE_URL+"offer/";
-  static const FAVOURITE =BASE_URL+"favourite/";
-  static const MART_ITEM_LIST = BASE_URL+"items/mart/";
-  static const HELP_API = BASE_URL+"help/";
-  ///catering section
-  static const CATERING_BANNER = BASE_URL+"banner_cat";
-  static const ADD_TO_CART = BASE_URL+"cart/";
-  static const CHECKOUT = BASE_URL+"checkout/";
-
-
-
+  static const BASE_URL="";
+  static const ImageUrl="";
+  static const sampleUrl="https://dribbble.com/shots/17903251-Mundur-M-Banking-App";
 }
